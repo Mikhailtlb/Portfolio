c4744c706f61946b10a8da51774a74daac064d5b
Mikailtaleb <mikhailtaleb235@gmail.com>
2025-05-16 23:47:29 +0200

    update map

--- JS/map.js
+++ JS/map.js
@@ -1,10 +1,12 @@
-// Maak een Leaflet-map met standaard marker
-let mapP = L.map('mapP1').setView([51.0424221, 3.7258331], 15);
+// Maak een Leaflet-map met standaard marker voor AP Hogeschool Campus Ellermanstraat
+let mapP = L.map('mapP1').setView([51.230050, 4.417340], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(mapP);
 
-// Voeg een standaard marker toe
-L.marker([51.0424221, 3.7258331]).addTo(mapP);
+// Voeg een standaard marker toe op de campuslocatie met popup
+L.marker([51.230050, 4.417340]).addTo(mapP)
+  .bindPopup("AP Hogeschool - Campus Ellermanstraat")
+  .openPopup();
