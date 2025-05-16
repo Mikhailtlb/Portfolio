78848c1365624e6dabc146b6bd1b45c3980fba28
Mikailtaleb <mikhailtaleb235@gmail.com>
2025-05-16 23:30:52 +0200

    update

---
+++ JS/map.js
@@ -0,0 +1,23 @@
+/* In dit script plaats je de code om de kaart in de id apMap te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat. De coördinaten van AP zijn: 51.23009 en 4.41616.
+Gebruik hiervoor de documentatie op https://leafletjs.com/ 
+*/
+
+let mapP = L.map('mapP1').setView([51.23009, 4.41616], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapP);
+
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+// plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
+let apMarker = L.marker([51.23009, 4.41616]).addTo(map);
+apMarker.bindPopup("<b>AP-Hogeschool</b><br>Ellermanstraat 33").openPopup();
+
